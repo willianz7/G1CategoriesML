80b7c9b153a9f68cd2a792fa5a3fd153a6ee587c
willianz7 <willian.victors@gmail.com>
2020-08-09 19:12:48 -0300

    meke it better

---
+++ my_env/bin/django-admin.py
@@ -0,0 +1,7 @@
+#!/home/willian/django-dev/my_env/bin/python3
+# EASY-INSTALL-DEV-SCRIPT: 'Django==3.2','django-admin.py'
+__requires__ = 'Django==3.2'
+__import__('pkg_resources').require('Django==3.2')
+__file__ = '/home/willian/django-dev/django/bin/django-admin.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
